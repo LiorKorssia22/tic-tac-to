ef1f6bbd9461d808d8dd01edfac0e070ff8aea78
Ariel Joseph <arieljoseph111@gmail.com>
2022-12-19 12:40:41 +0200

    Added method markSquare

--- src/index.js
+++ src/index.js
@@ -1,92 +1,88 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const rowStyle = {
-  display: 'flex'
-}
+  display: "flex",
+};
 
 const boxStyle = {
-  'width':'60px',
-  'height':'60px',
-  'backgroundColor': '#ddd',
-  'margin': '4px',
-  'display': 'flex',
-  'justifyContent': 'center',
-  'alignItems': 'center',
-  'fontSize': '20px',
-  'color': 'white'
-}
+  width: "60px",
+  height: "60px",
+  backgroundColor: "#ddd",
+  margin: "4px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  fontSize: "20px",
+  color: "white",
+};
 
 const boardStyle = {
-  'backgroundColor': '#eee',
-  'width': '208px',
-  'alignItems': 'center',
-  'justifyContent': 'center',
-  'display': 'flex',
-  'flexDirection': 'column',
-  'border': '3px #eee solid'
-}
+  backgroundColor: "#eee",
+  width: "208px",
+  alignItems: "center",
+  justifyContent: "center",
+  display: "flex",
+  flexDirection: "column",
+  border: "3px #eee solid",
+};
 
 const containerStyle = {
-  'display': 'flex',
-  'alignItems': 'center',
-  'flexDirection': 'column'
-}
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+};
 
 const messageStyle = {
-  'marginTop': '5px',
-  'marginBottom': '5px',
-  'fontWeight': 'bold',
-  'fontSize': '16px',
-}
+  marginTop: "5px",
+  marginBottom: "5px",
+  fontWeight: "bold",
+  fontSize: "16px",
+};
 
 const buttonStyle = {
-  'marginTop': '15px',
-  'marginBottom': '16px',
-  'width': '80px',
-  'height': '40px',
-  'backgroundColor': '#8acaca',
-  'color': 'white',
-  'fontSize': '16px',
-}
+  marginTop: "15px",
+  marginBottom: "16px",
+  width: "80px",
+  height: "40px",
+  backgroundColor: "#8acaca",
+  color: "white",
+  fontSize: "16px",
+};
 
 function Square() {
-  return (
-    <div
-      className="square"
-      style={boxStyle}>
-    </div>
-  );
+  return <div className="square" style={boxStyle}></div>;
 }
 
-
 function Board() {
-
+  const markSquare = (num) => {
+    console.log(num);
+  };
   return (
     <div style={containerStyle} className="tttBoard">
-      <div id="statusArea" className="status" style={messageStyle}>Current player: <span>X</span></div>
-      <div id="winnerArea" className="winner" style={messageStyle}>Winner: <span>None</span></div>
+      <div id="statusArea" className="status" style={messageStyle}>
+        Current player: <span>X</span>
+      </div>
+      <div id="winnerArea" className="winner" style={messageStyle}>
+        Winner: <span>None</span>
+      </div>
       <div style={boardStyle}>
         <div className="board-row" style={rowStyle}>
-          {/* <Square onClick={() => markSquare(0)}/> */}
-          <Square />
-          <Square />
-        </div>
-      <div className="board-row" style={rowStyle}>
-          <Square />
-          <Square />
+          <Square onClick={() => markSquare(0)} />
           <Square />
         </div>
-         <div className="board-row" style={rowStyle}>
+        <div className="board-row" style={rowStyle}>
           <Square />
           <Square />
           <Square />
         </div>
-       
+        <div className="board-row" style={rowStyle}>
+          <Square />
+          <Square />
+          <Square />
+        </div>
       </div>
       <button style={buttonStyle}>Reset</button>
-
     </div>
   );
 }
@@ -101,8 +97,4 @@ function TicTacToe() {
   );
 }
 
-ReactDOM.render(
-  <TicTacToe />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<TicTacToe />, document.getElementById("root"));
